9aa83d85eff876b2b546031b1dba653f9febe120
Emmanuel <emmasco0207@gmail.com>
2026-02-02 00:21:05 +0100

    added color customization

--- src/components/UI/SideNav.jsx
+++ src/components/UI/SideNav.jsx
@@ -1,11 +1,9 @@
+import Customization from "./Customization";
+
 const SideNav = () => {
   return (
     <section className="w-full h-full bg-white flex flex-col p-6 shadow-lg">
-      <h1 className="text-xl font-semibold mb-4">Edit car model</h1>
-
-      <div>
-        kd
-      </div>
+      <Customization/>
     </section>
   );
 };
